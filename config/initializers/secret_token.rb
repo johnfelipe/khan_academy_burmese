1f8810082d3ed3dc96efeeb65b1a53b2b33fb3d1
Yoni Doron-Peters <yonidoronpeters@berkeley.edu>
2013-10-16 18:21:44 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KABT::Application.config.secret_token = '7d61f4ee6cb97ab6c3dc6acd732d2d13836244382b12316de4dc486dbd2ee685cc70082a348fbfefd1c9c8cdba38be5ae6ec42ca819bea4af50b3e74f1238558'
