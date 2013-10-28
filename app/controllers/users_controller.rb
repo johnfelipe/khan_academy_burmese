f4b222d1bd55728c80fd5baf73addd5eb4121890
Yoni Doron-Peters <yonidoronpeters@berkeley.edu>
2013-10-27 22:52:49 -0700

    working login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,8 @@ class UsersController < ApplicationController
     @user = User.create!(userHash)
     flash[:notice] = "Your account was successfully created"
     #Redirect to dashboard?
-    redirect_to show_dashboard_path(@user)
+    #redirect_to user_path(@user)
+    redirect_to login_page_path
   end
 
   def edit
@@ -84,6 +85,7 @@ class UsersController < ApplicationController
       redirect_to login_page_path
     end
   end
+  
   def dashboard
     if session[:id].nil?
       redirect_to login_page_path
