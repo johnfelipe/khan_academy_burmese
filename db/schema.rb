04c56a5999398fa800a36292aecd47d44d3cab8f
Yoni Doron-Peters <yonidoronpeters@berkeley.edu>
2013-11-10 17:28:34 -0800

    add index on user email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131028032438) do
+ActiveRecord::Schema.define(:version => 20131111012618) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(:version => 20131028032438) do
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
   create_table "videos", :force => true do |t|
     t.string   "video_id"
