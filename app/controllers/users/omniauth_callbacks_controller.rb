6e95e59e8582f29dbc6c0a6941ee339ea99cb9be
James Ferguson <jferguson@berkeley.edu>
2013-11-10 20:36:16 -0800

    Fixed 3rd part login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -32,6 +32,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
       redirect_to show_dashboard_path(@user)
+      session[:id] = @user.id
         #sign_in_and_redirect @user, :event => :authentication
     else
       session["devise.google_data"] = request.env["omniauth.auth"]
@@ -44,6 +45,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     if @user.persisted?
       #sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      session[:id] = @user.id
       redirect_to show_dashboard_path(@user)
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
