396425ce4aeb8785375d35b737739604dc34c6a7
Asaf <asafaa32@gmail.com>
2013-12-01 21:52:10 -0800

    mail domain change

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,8 +1,8 @@
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :domain               => "gmail.com",
-  :user_name            => "username@gmail.com",
+  :domain               => "railscasts.com",
+  :user_name            => "railscasts",
   :password             => "secret",
   :authentication       => "plain",
   :enable_starttls_auto => true
