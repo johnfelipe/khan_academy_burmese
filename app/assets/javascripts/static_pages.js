633ca4aaa39341e0537f3cd894d92432af8da920
James Ferguson <jferguson@berkeley.edu>
2013-12-07 19:11:17 -0800

    Import videos

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -13,4 +13,19 @@ $(document).ready(function() {
 			navbar.toggleClass('video-nav');
 		};
 	});
+    $("#import_videos").click(show_import);
+    $("#upload_csv").click(warn_length);
 });
+
+function show_import() {
+    //Show new pop-up allowing users to input a csv file
+    $("#import_videos_form").css("position", "fixed");
+    $("#import_videos_form").toggle();    
+    return false;
+}
+
+function warn_length() {
+    alert("This operation may take a while, especially if importing lots of videos.");
+    return true;
+}
+
